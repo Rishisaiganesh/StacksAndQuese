dfba3f8a89826f29fe8fd9b31929c9db585f1994
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-18 14:40:24 +0530

    [Rishi]Adding:peekAndPop

--- StacksQuese/StacksAndQuese/Program.cs
+++ StacksQuese/StacksAndQuese/Program.cs
@@ -13,6 +13,13 @@ namespace StacksAndQuese
             stack.Push(56);
             Console.WriteLine("stacklist Elements");
             stack.Display();
+            stack.Pop();
+            Console.WriteLine("List After Poping");
+            stack.Display();
+            stack.Peek();
+            Console.WriteLine("list after Peek");
+            stack.Display();
+            stack.Size();
         }
     }
 }
